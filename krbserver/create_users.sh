866b4ed2a9836adbec9f10ef61f1391383b4bae8
isx47590131 <arnauesteban@hotmail.com>
2018-04-04 12:48:05 +0200

    Kerberos host

--- krbserver/create_users.sh
+++ krbserver/create_users.sh
@@ -2,5 +2,7 @@ kadmin.local -q "addprinc arnau" -w karnau
 kadmin.local -q "addprinc pere" -w kpere
 kadmin.local -q "addprinc pere/admin" -w kpere
 kadmin.local -q "addprinc pau" -w kpau
+kadmin.local -q "addprinc marta" -w kmarta
+kadmin.local -q "addprinc jordi" -w kjordi
 
 
---
+++ sshkserver/README.md
@@ -0,0 +1,3 @@
+# server ssh configurado para kerberos
+
+
